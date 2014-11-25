724b69aca04b540eb2e09f4e01ea6dba069ee368
mkpc <mkpc@uw.edu>
2014-11-25 12:05:52 -0800

    class done

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,28 @@
 
 'use strict';
 
+//var http = require('http');
+//
+//var server = http.createServer(function(req, res){
+//    res.writeHead(200,{'Content-Type':'text/plain'});
+//    res.end('Hello World from Marco!\n');
+//});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World from Marco!');
+});
+
+app.get('/foo', function(req,res){
+    res.send('You requested /foo');
+});
+
+app.listen(8080, function(){
+    console.log('Server listening on http://localhost:8080');
+});
+//
+//server.listen(8080, function(){
+//    console.log('Server listening on http://localhost:8080');
+//});
